bc51161dc41e10271ff8b04c9943d5025c9eb430
Jacques Hirtzel <jacques@hirtzel.ch>
2020-03-18 09:02:51 +0100

    ignore config + config.exemple

---
+++ config.exemple.php
@@ -0,0 +1,12 @@
+<?php
+# CMS Made Simple Configuration File
+# Documentation: https://docs.cmsmadesimple.org/configuration/config-file/config-reference
+#
+$config['dbms'] = 'mysqli';
+$config['db_hostname'] = 'localhost';
+$config['db_username'] = 'root';
+$config['db_password'] = '123456';
+$config['db_name'] = 'covicou_cms';
+$config['db_prefix'] = 'cms_';
+$config['timezone'] = 'Europe/Zurich';
+?>
